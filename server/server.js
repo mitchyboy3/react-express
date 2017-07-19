c38c97d66eda0fa63df5753b820b170a6c08663b
Mitchel Foster <mitchfoster3@gmail.com>
2017-07-19 14:08:09 -0600

    Notes on the code Joe shared

--- server/server.js
+++ server/server.js
@@ -13,23 +13,33 @@ app.use(bodyParser.json());
 app.use(session({
   resave: true, 
   saveUninitialized: true, 
-  secret: config.sessionSecret
+  secret: config.sessionSecret //ANY RANDOM STING/SECRET
 }))
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-passport.use(new Auth0Strategy({
-   domain:       config.auth0.domain,
-   clientID:     config.auth0.clientID,
-   clientSecret: config.auth0.clientSecret,
+passport.use(new Auth0Strategy({      //OBJECT THAT CONTAINS INFO FORM AUTH0; COPY EXACTLY
+   domain:       config.auth0.domain, //OBTAINED FROM AUTH0.COM
+   clientID:     config.auth0.clientID, //OBTAINED FROM AUTH0.COM
+   clientSecret: config.auth0.clientSecret, //OBTAINED FROM AUTH0.COM
    callbackURL:  config.auth0.callbackUrl
   },
-  function(accessToken, refreshToken, extraParams, profile, done) {
+  function(accessToken, refreshToken, extraParams, profile, done) { //PARAMS IN THIS ORDER EVERYTIME
+      //ANYDATABASE CALLS
+
+      // db.getUser([profile.auth0.id]).then((err, user) => {
+      //   if (user) {
+      //     done(null, user[0])
+      //   } else {
+      //     db.createUser().then((err, user) => {
+      //       done(null, user[0])
+      //     })
+      //   }
+      // })
 
 
-
-    done(null, { id: 2, username: 'Joe', email: 'joe@joe.com' })
+    done(null, { id: 2, username: 'Joe', email: 'joe@joe.com' }) //USUALLY THE USER; HARDCODED
   }
 ));
 
